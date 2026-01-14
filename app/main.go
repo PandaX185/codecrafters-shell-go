3ae278552d6ffa9af647db5de9fd4db65cf3ac7a
PandaX185 <mouss3abbb@gmail.com>
2026-01-14 21:39:09 +0200

    stage-3: add repl support

--- app/main.go
+++ app/main.go
@@ -7,12 +7,13 @@ import (
 )
 
 func main() {
-	fmt.Print("$ ")
-	cmd, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Println("Error reading command:", err)
-		os.Exit(1)
+	for {
+		fmt.Print("$ ")
+		cmd, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading command:", err)
+			continue
+		}
+		fmt.Printf("%v: command not found\n", cmd[:len(cmd)-1])
 	}
-	fmt.Printf("%v: command not found\n", cmd[:len(cmd)-1])
-
 }
