7df4894341853e8d86926041d8c8f9408ff34d94
PandaX185 <mouss3abbb@gmail.com>
2026-01-14 21:37:45 +0200

    stage-2: add command not found handler

--- app/main.go
+++ app/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
 	fmt.Print("$ ")
+	cmd, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading command:", err)
+		os.Exit(1)
+	}
+	fmt.Printf("%v: command not found\n", cmd[:len(cmd)-1])
+
 }
