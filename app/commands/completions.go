29e1e150be84047ba395bd2a8cf0f07682f8671f
PandaX185 <mouss3abbb@gmail.com>
2026-01-15 20:35:06 +0200

    refactor: format import statements and improve code structure in GetCompletions function

--- app/commands/completions.go
+++ app/commands/completions.go
@@ -1,12 +1,23 @@
 package commands
 
-import "strings"
+import (
+	"strings"
+)
 
 func GetCompletions(prefix string) []string {
 	var completions []string
 	for cmd := range builtinCommands {
 		if strings.HasPrefix(cmd, prefix) {
 			completions = append(completions, cmd)
+		}
+	}
+
+	executables := getPathFiles()
+	for _, exe := range executables {
+		parts := strings.Split(exe, "/")
+		name := parts[len(parts)-1]
+		if strings.HasPrefix(name, prefix) {
+			completions = append(completions, name)
 		}
 	}
 	return completions
