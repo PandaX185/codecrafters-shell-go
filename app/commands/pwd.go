89f5215377b74a8aea89303df44c1c0e96c8c751
PandaX185 <mouss3abbb@gmail.com>
2026-01-19 03:55:35 +0200

    fix: ensure newline at the end of output for echo, pwd, and type commands

--- app/commands/pwd.go
+++ app/commands/pwd.go
@@ -6,7 +6,7 @@ import (
 
 func handlePwd() (string, string) {
 	if dir, err := os.Getwd(); err == nil {
-		return dir, ""
+		return dir + "\n", ""
 	} else {
 		return "", "pwd: error retrieving current directory"
 	}
